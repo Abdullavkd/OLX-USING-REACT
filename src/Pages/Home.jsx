9f25af2ee5a02716e6967332d89fee65b5b7bee6
Abdulla Vennakode <abdullacp9048@gmail.com>
2026-01-28 17:04:49 +0530

    added key for items in app.js with map()

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,12 +4,11 @@ import ProductCard from '../Components/ProductCard';
 
 const Home = () => {
     const {product} = useContext(ProductsContext)
-    console.log(product)
   return (
     <div>
         <div className='m-auto grid gap-5 mt-13 w-full sm:w-133 sm:grid-cols-2 md:w-203 md:grid-cols-3 lg:w-271 lg:grid-cols-4 xl:grid-cols-5 xl:w-331'>
             {product.map(val => (
-                <ProductCard price={val.price} title={val.title} date={val.date} img={val.image}/>
+                <ProductCard key={val.id} price={val.price} title={val.title} date={val.date} img={val.image}/>
             ))}
         </div>
     </div>
